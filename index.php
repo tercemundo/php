dd10e972e2296ce91b13a1fe82f11eda69776aea
tercemundo <94326261+tercemundo@users.noreply.github.com>
2021-11-19 14:26:29 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$ip=getenv('MYSQL_SERVICE_HOST');
+$pass=getenv('MYSQL_PASS')
+ $mysqli = new mysqli( $ip , 'root', $pass , 'mydatabase');
+ // If an error occurs when establishing the connection
+ if ($mysqli->connect_errno) {
+ // We can show a message:
+ echo "An error occurred when trying to establish a
+connection to the database: ";
+ // And this is the detail of the error message.
+ echo "Error " . "#".$mysqli->connect_errno . "\n";
+ exit;
+ }
+ else {
+ // Query to retrieve Orders table data
+ $select_query = "SELECT id, name, supplier, category, price,
+instock FROM products";
+ $result = $mysqli->query($select_query);
+ while ($obj = mysqli_fetch_object($result)) {// call to
+mysqli_fetch_object function
+ echo "ID: ".$obj->id.", ".
+ "Name: ".$obj->name.", ".
+ "Supplier: ".$obj->supplier.", ".
+ "Category: ".$obj->category.", ".
+ "Price: ".$obj->price.", ".
+ "In Stock: ".$obj->instock."<br>";
+ }
+ }
+?>
